b926e364ac23df572d9ad3568b164ed9fae2dff4
Vinicius Silva <vsaguiar@outlook.com>
2023-08-29 20:18:12 -0300

    Criando o arquivo de contexto do EF Core

---
+++ CatalogoAPI/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CatalogoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace CatalogoAPI.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+}
