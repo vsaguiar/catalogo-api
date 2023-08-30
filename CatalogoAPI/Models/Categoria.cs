bcb57f6697e75b955fae35278af0f6c55af3c6ad
Vinicius Silva <vsaguiar@outlook.com>
2023-08-29 21:21:45 -0300

    Definindo o relacionamento entre as entidades

--- CatalogoAPI/Models/Categoria.cs
+++ CatalogoAPI/Models/Categoria.cs
@@ -1,8 +1,15 @@
-﻿namespace CatalogoAPI.Models;
+﻿using System.Collections.ObjectModel;
+
+namespace CatalogoAPI.Models;
 
 public class Categoria
 {
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
     public int CategoriaId { get; set; }
     public string Nome { get; set; }
     public string ImagemUrl { get; set; }
+    public ICollection<Produto> Produtos { get; set; }
 }
