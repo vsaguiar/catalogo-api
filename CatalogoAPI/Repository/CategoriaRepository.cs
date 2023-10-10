cd826732c2abd0326dd3d34dfb3d8b7a70a6d034
Vinicius Silva <vsaguiar@outlook.com>
2023-10-10 20:30:02 -0300

    Produtos e Categorias no padrão repositório

---
+++ CatalogoAPI/Repository/CategoriaRepository.cs
@@ -0,0 +1,15 @@
+﻿using CatalogoAPI.Context;
+using CatalogoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogoAPI.Repository;
+
+public class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
+{
+    public CategoriaRepository(AppDbContext contexto) : base(contexto) { }
+
+    public IEnumerable<Categoria> GetCategoriasProdutos()
+    {
+        return Get().Include(c => c.Produtos);
+    }
+}
---
+++ CatalogoAPI/Repository/ProdutoRepository.cs
@@ -0,0 +1,16 @@
+﻿using CatalogoAPI.Context;
+using CatalogoAPI.Models;
+
+namespace CatalogoAPI.Repository;
+
+public class ProdutoRepository : Repository<Produto>, IProdutoRepository
+{
+    public ProdutoRepository(AppDbContext contexto) : base(contexto)
+    {
+    }
+
+    public IEnumerable<Produto> GetProdutosPorPrecos()
+    {
+        return Get().OrderBy(p => p.Preco).ToList();
+    }
+}
