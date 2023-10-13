cf4bbda37ba83bb3ac5c5d7aa09dc0ca4296636a
Vinicius Silva <vsaguiar@outlook.com>
2023-10-13 18:20:43 -0300

    Ajustando o mapeamento via AutoMapper

--- CatalogoAPI/Controllers/ProdutosController.cs
+++ CatalogoAPI/Controllers/ProdutosController.cs
@@ -1,4 +1,6 @@
-﻿using CatalogoAPI.Filters;
+﻿using AutoMapper;
+using CatalogoAPI.DTOs;
+using CatalogoAPI.Filters;
 using CatalogoAPI.Models;
 using CatalogoAPI.Repository;
 using Microsoft.AspNetCore.Mvc;
@@ -11,49 +13,60 @@ public class ProdutosController : ControllerBase
 {
 
     private readonly IUnitOfWork _uof;
-    public ProdutosController(IUnitOfWork context)
+    private readonly IMapper _mapper;
+    public ProdutosController(IUnitOfWork context, IMapper mapper)
     {
         _uof = context;
+        _mapper = mapper;
     }
 
 
     [HttpGet("menorpreco")]
-    public ActionResult<IEnumerable<Produto>> GetProdutosPrecos()
+    public ActionResult<IEnumerable<ProdutoDTO>> GetProdutosPrecos()
     {
-        return _uof.ProdutoRepository.GetProdutosPorPrecos().ToList();
+        var produtos = _uof.ProdutoRepository.GetProdutosPorPrecos().ToList();
+        var produtosDTO = _mapper.Map<List<ProdutoDTO>>(produtos);
+        return produtosDTO;
     }
 
 
     [HttpGet]
     [ServiceFilter(typeof(ApiLoggingFilter))]
-    public ActionResult<IEnumerable<Produto>> Get()
+    public ActionResult<IEnumerable<ProdutoDTO>> Get()
     {
         var produtos = _uof.ProdutoRepository.Get().ToList();
+
         if (produtos is null)
         {
             return NotFound("Produtos não encontrados...");
         }
 
-        return produtos;
+        var produtosDTO = _mapper.Map<List<ProdutoDTO>>(produtos);
+
+        return produtosDTO;
     }
 
 
     [HttpGet("{id:int:min(1)}", Name ="ObterProduto")] // Restrição de rota com o valor mínimo para evitar consulta desnecessária no banco
-    public ActionResult<Produto> Get(int id)
+    public ActionResult<ProdutoDTO> Get(int id)
     {
         var produtos = _uof.ProdutoRepository.GetById(p => p.ProdutoId == id);
+
         if (produtos is null)
         {
             return NotFound("Produto não encontrado.");
         }
 
-        return produtos;
+        var produtosDTO = _mapper.Map<ProdutoDTO>(produtos);
+
+        return produtosDTO;
     }
 
 
     [HttpPost]
-    public ActionResult Post(Produto produto)
+    public ActionResult Post([FromBody] ProdutoDTO produtoDTO)
     {
+        var produto = _mapper.Map<Produto>(produtoDTO);
         if (produto is null)
         {
             return BadRequest();
@@ -61,17 +74,21 @@ public class ProdutosController : ControllerBase
         _uof.ProdutoRepository.Add(produto);
         _uof.Commit();
 
-        return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+        var produtoDto = _mapper.Map<ProdutoDTO>(produtoDTO);
+
+        return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produtoDto);
     }
 
 
     [HttpPut("{id:int}")]
-    public ActionResult Put(int id, Produto produto)
+    public ActionResult Put(int id, [FromBody] ProdutoDTO produtoDTO)
     {
-        if (id != produto.ProdutoId)
+        if (id != produtoDTO.ProdutoId)
         {
             return BadRequest();
         }
+
+        var produto = _mapper.Map<Produto>(produtoDTO);
 
         _uof.ProdutoRepository.Update(produto);
         _uof.Commit();
@@ -81,7 +98,7 @@ public class ProdutosController : ControllerBase
 
 
     [HttpDelete("{id:int}")]
-    public ActionResult Delete(int id)
+    public ActionResult<ProdutoDTO> Delete(int id)
     {
         var produto = _uof.ProdutoRepository.GetById(p => p.ProdutoId == id);
 
@@ -93,7 +110,9 @@ public class ProdutosController : ControllerBase
         _uof.ProdutoRepository.Delete(produto);
         _uof.Commit();
 
-        return Ok(produto);
+        var produtoDTO = _mapper.Map<ProdutoDTO>(produto);
+
+        return produtoDTO;
     }
 
 }
