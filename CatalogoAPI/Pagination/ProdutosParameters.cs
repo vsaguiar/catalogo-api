2aa86140f052694fb82c4e8b8a30b8e613eb797d
Vinicius Silva <vsaguiar@outlook.com>
2023-10-17 00:24:58 -0300

    Otimizando o código para não ter duplicidade

--- CatalogoAPI/Pagination/ProdutosParameters.cs
+++ CatalogoAPI/Pagination/ProdutosParameters.cs
@@ -1,19 +1,5 @@
 ﻿namespace CatalogoAPI.Pagination;
 
-public class ProdutosParameters
+public class ProdutosParameters : QueryStringParameters
 {
-    const int maxPageSize = 50;
-    public int PageNumber { get; set; } = 1;
-    public int _pageSize = 10;
-    public int PageSize
-    {
-        get
-        {
-            return _pageSize;
-        }
-        set
-        {
-            _pageSize = (value > maxPageSize) ? maxPageSize : value;
-        }
-    }
 }
